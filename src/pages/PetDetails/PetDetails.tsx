7e6a45b6e6eedb6b22aab8de3adc22b5f3485b28
Vinicius <vinylacerda0501@hotmail.com>
2024-04-16 22:22:55 -0300

    Criando o botão de voltar do header

--- src/pages/PetDetails/PetDetails.tsx
+++ src/pages/PetDetails/PetDetails.tsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid/Grid";
-import styles from "./Pets.module.css"
+import styles from "./PetDetails.module.css";
 
-export function Pets() {
+
+export function PetDetails() {
     return (
         <Grid>
             <div className={styles.container}>
-                <Header />
-                <Link to='/pets/20'>Ir para listagens</Link>
+                <Header showReturn={true} />
+                <h1>Detalhes do pet</h1>
             </div>
         </Grid>
     )
