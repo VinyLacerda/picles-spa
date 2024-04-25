d757270dd3efb95e50058e4a304e9e04c5c50cc0
Vinicius <vinylacerda0501@hotmail.com>
2024-04-24 21:24:26 -0300

    desenvolvemos o formulario pegando dados do shelter, instalamos a biblioteca use mask input.

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,21 +1,80 @@
-import { useRef } from "react";
-import { Button } from "../../../components/common/Button";
-import { Panel } from "../../../components/layout/Panel";
-import styles from "./Shelter.module.css"
-import {Input} from "../../../components/common/Input"
+import { useForm } from 'react-hook-form'
+import { Panel } from '../../../components/layout/Panel'
+import styles from './Shelter.module.css'
+import { Input } from '../../../components/common/Input'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '../../../components/common/Button'
+import { useHookFormMask } from 'use-mask-input'
+
+const shelterSchema = z.object({
+    name: z.string()
+    .min(2, 'Nome deve ter no mínimo 2 caracteres.')
+    .max(30, 'Nome deve ter no máximo 30 caracteres'),
+    email: z.string().email('Campo deve ser um email.'),
+    phone: z.string().refine((value) => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >=10 && digits <= 11
+    }),
+    whatsApp: z.string().refine((value) => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >=10 && digits <= 11
+    })
+})
+
+type ShelterSchema = z.infer<typeof shelterSchema>
 
 export function Shelter() {
-    const inputRef = useRef(null)
+    const {handleSubmit, register, formState} = useForm<ShelterSchema>({
+        resolver: zodResolver(shelterSchema),
+    })
 
-    function submit(event) {
-        event.preventDefault()
-        console.log(inputRef.current.value)
+    const registerWithMask = useHookFormMask(register)
+    
+    function submit ({name}: ShelterSchema) {
+        console.log(name)
     }
+
     return (
         <Panel>
-            <form className={styles.container}>
-            <Input label="Nome" />
-            <Button type={"submit"}>Salvar</Button>
+            <form className={styles.container} onSubmit={handleSubmit(submit)}>
+                <div>
+                <Input label="Nome" {...register('name')} />
+                    {
+                     formState.errors?.name && 
+                     <p className={styles.formError}>
+                        {formState.errors.name.message}</p>
+                    }
+                </div>
+
+                <div>
+                <Input label="Email" {...register('email')} />
+                    {
+                     formState.errors?.email && 
+                     <p className={styles.formError}>
+                        {formState.errors.email.message}</p>
+                    }
+                </div>
+
+                <div>
+                <Input required label="Telefone" {...registerWithMask('phone', ['99 9999-9999', '99 99999-9999'])} />
+                    {
+                     formState.errors?.phone && 
+                     <p className={styles.formError}>
+                        {formState.errors.phone.message}</p>
+                    }
+                </div>
+
+                <div>
+                <Input required label="WhatsApp" {...registerWithMask('whatsApp', ['99 9999-9999', '99 99999-9999'])} />
+                    {
+                     formState.errors?.whatsApp && 
+                     <p className={styles.formError}>
+                        {formState.errors.whatsApp.message}</p>
+                    }
+                </div>
+
+                <Button type="submit">Salvar dados</Button>
             </form>
         </Panel>
     )
